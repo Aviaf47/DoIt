26c18b3b20ada993f0dc548703a7dbdff64b3da3
user120718 <user120718@NY213.local>
2017-04-01 19:42:31 -0700

    no message

---
+++ DoIt!/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt!
+//
+//  Created by user120718 on 3/29/17.
+//  Copyright © 2017 Aviaf Apps. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
